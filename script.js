04f58a6d0603e9cd206fb1e8982148bc4022a1d3
DovydasMM <dovistk@gmail.com>
2022-06-08 09:51:04 +0300

    Created function for winner check

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function gameBoard(){
+        //create gameboard
         let board = [];
         for (i=0; i<9; i++){
-            board.push('');
+            board.push(i);
         }
 
         //drawing cell elements
@@ -10,16 +11,46 @@ function gameBoard(){
             const cell = document.createElement('div');
             cell.className='cell';
             cell.addEventListener('click', () => {
-                if (board[index]==''){
-                    board[index]='x';
+                if (board[index]!=null){
+                    board[index]='X';
                     cell.textContent=board[index];
-                    console.log(board);
+                    winnerCheck()
             }});
             container.appendChild(cell)
 
         }
         )
+        //function for winner check
+        function winnerCheck(){
+            const winnerIndex = [[0,1,2], 
+                                [3,4,5],
+                                [6,7,8],
+                                [0,3,6],
+                                [1,4,7],
+                                [2,5,8],
+                                [0,4,8],
+                                [2,4,6]]
+            for (i=0; i<winnerIndex.length;i++){
+                   if (board[winnerIndex[i][0]]==board[winnerIndex[i][1]] && board[winnerIndex[i][0]] == board[winnerIndex[i][2]] && board[winnerIndex[i][1]]==board[winnerIndex[i][2]]){
+                    console.log(`Winner is ${playerOne.name}!`)
+                    }
+                }
+            }
+            
         return {board}
     }
+
+
+
+function playerCreate(name, symbol){
+    name,
+    symbol
+    return {name, symbol}
+}
+const playerOne = playerCreate('Dovydas','X');
+const playerTwo = playerCreate('Simona', '0')
 const lenta = gameBoard();
 
+function currentPlayer(player1, player2){
+
+}
